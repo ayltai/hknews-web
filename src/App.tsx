fe0b65614e38b8c849b8d5163b34af66781bbcd8
Alan Tai <ayltai@gmail.com>
2019-09-19 15:23:11 +0800

    Support responsive Card UI width

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 
 import * as Config from './Config';
@@ -11,16 +13,22 @@ interface IProps {
 }
 
 export const App : React.FunctionComponent<IProps> = (props : any) : React.ReactElement => (
-    <Admin
-        dataProvider={dataProvider}
-        i18nProvider={() : object => require('./lang').lang}
-        {...props}>
-        {Object.entries(Config.CATEGORIES).map((entry : any[number]) => (
-            <MainResource
-                key={entry[0]}
-                name={entry[0]}
-                icon={entry[1]} />
-        ))}
-        <Resource name='sources' />
-    </Admin>
+    <ThemeProvider theme={createMuiTheme({
+        palette : {
+            type : 'light',
+        },
+    })}>
+        <Admin
+            dataProvider={dataProvider}
+            i18nProvider={() : object => require('./lang').lang}
+            {...props}>
+            {Object.entries(Config.CATEGORIES).map((entry : any[number]) => (
+                <MainResource
+                    key={entry[0]}
+                    name={entry[0]}
+                    icon={entry[1]} />
+            ))}
+            <Resource name='sources' />
+        </Admin>
+    </ThemeProvider>
 );
--- src/view/list/CardList.tsx
+++ src/view/list/CardList.tsx
@@ -1,8 +1,5 @@
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
+import { Card, CardActions, CardContent, CardHeader, CardMedia, Theme } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 import React from 'react';
 import TimeAgo from 'react-timeago';
 
@@ -10,63 +7,117 @@ import './CardList.css';
 
 const { TextField, RichTextField, ShowButton } = require('react-admin');
 
-const avatarSize : number = 40;
-
 export interface IProps {
     basePath? : string;
+    classes?  : any;
     data?     : any;
     ids?      : string[];
 }
 
-export const CardList : React.FunctionComponent<IProps> = (props : IProps) : React.ReactElement => (
-    <div>
-        {props.ids!.map((id : string) => (
-            <Card
-                className='Card'
-                key={id}>
-                <CardHeader
-                    title={
-                        <TextField
+export interface IStyles {
+    card                    : {};
+    descriptionWithMedia    : {};
+    descriptionWithoutMedia : {};
+}
+
+const styles : (theme : Theme) => IStyles = (theme : Theme) : IStyles => ({
+    card                    : {
+        [theme.breakpoints.down('xs')] : {
+            height : 480,
+            margin : '1.25em',
+            width  : 320,
+        },
+        [theme.breakpoints.only('sm')] : {
+            height : 560,
+            margin : '1.5em',
+            width  : 480,
+        },
+        [theme.breakpoints.up('md')]   : {
+            height : 640,
+            margin : '2em',
+            width  : 480,
+        },
+    },
+    descriptionWithMedia    : {
+        [theme.breakpoints.down('xs')] : {
+            height : 70,
+        },
+        [theme.breakpoints.only('sm')] : {
+            height : 65,
+        },
+        [theme.breakpoints.up('md')]   : {
+            height : 140,
+        },
+    },
+    descriptionWithoutMedia : {
+        [theme.breakpoints.down('xs')] : {
+            height : 250,
+        },
+        [theme.breakpoints.only('sm')] : {
+            height : 330,
+        },
+        [theme.breakpoints.up('md')]   : {
+            height : 410,
+        },
+    },
+});
+
+const avatarSize : number = 40;
+
+const RawCardList : React.FunctionComponent<IProps> = (props : IProps) : React.ReactElement => {
+    const { classes } = props;
+
+    return (
+        <div>
+            {props.ids!.map((id : string) => (
+                <Card
+                    key={id}
+                    className={`Card ${classes.card}`}>
+                    <CardHeader
+                        title={
+                            <TextField
+                                record={props.data[id]}
+                                source='source.name' />
+                        }
+                        subheader={
+                            <TimeAgo date={props.data[id].publishDate} />
+                        }
+                        avatar={
+                            <img
+                                src={`${process.env.REACT_APP_API_ENDPOINT + props.data[id].source.imageUrl}`}
+                                width={avatarSize}
+                                height={avatarSize}
+                                alt={props.data[id].source.name}
+                                title={props.data[id].source.name} />
+                        } />
+                    {props.data[id].images && props.data[id].images.length > 0 && (
+                        <CardMedia
+                            className='Media'
+                            image={props.data[id].images[0].imageUrl}
+                            title={props.data[id].images[0].description} />
+                    )}
+                    <CardContent>
+                        <RichTextField
+                            className='Title'
                             record={props.data[id]}
-                            source='source.name' />
-                    }
-                    subheader={
-                        <TimeAgo date={props.data[id].publishDate} />
-                    }
-                    avatar={
-                        <img
-                            src={`${process.env.REACT_APP_API_ENDPOINT + props.data[id].source.imageUrl}`}
-                            width={avatarSize}
-                            height={avatarSize}
-                            alt={props.data[id].source.name}
-                            title={props.data[id].source.name} />
-                    } />
-                {props.data[id].images && props.data[id].images.length > 0 && (
-                    <CardMedia
-                        className='Media'
-                        image={props.data[id].images[0].imageUrl}
-                        title={props.data[id].images[0].description} />
-                )}
-                <CardContent>
-                    <RichTextField
-                        className='Title'
-                        record={props.data[id]}
-                        source='title' />
-                </CardContent>
-                <CardContent>
-                    <RichTextField
-                        className='Description'
-                        style={props.data[id].images && props.data[id].images.length > 0 ? { height : '140px' } : { height : '410px' }}
-                        record={props.data[id]}
-                        source='description' />
-                </CardContent>
-                <CardActions className='Actions'>
-                    <ShowButton
-                        label='labels.read_more'
-                        basePath={props.basePath}
-                        record={props.data[id]} />
-                </CardActions>
-            </Card>
-        ))}
-    </div>
-);
+                            source='title' />
+                    </CardContent>
+                    <CardContent>
+                        <RichTextField
+                            className={`Description ${props.data[id].images && props.data[id].images.length > 0 ? classes.descriptionWithMedia : classes.descriptionWithoutMedia}`}
+                            record={props.data[id]}
+                            source='description' />
+                    </CardContent>
+                    <CardActions className='Actions'>
+                        <ShowButton
+                            label='labels.read_more'
+                            basePath={props.basePath}
+                            record={props.data[id]} />
+                    </CardActions>
+                </Card>
+            ))}
+        </div>
+    );
+};
+
+export const CardList : any = withStyles(styles)(RawCardList);
