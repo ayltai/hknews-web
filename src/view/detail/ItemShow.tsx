db6cde51d745da2b681edec046a30db3424ddba3
Alan Tai <ayltai@gmail.com>
2019-09-23 13:29:26 +0800

    Auto scroll to top on details page

--- src/view/detail/ItemShow.tsx
+++ src/view/detail/ItemShow.tsx
@@ -8,40 +8,56 @@ const Title : ({ record } : any) => React.ReactElement = ({ record } : any) : Re
     <span>{record ? `${record.title}` : ''}</span>
 );
 
-export const ItemShow : (props : any) => React.ReactElement = (props : any) : React.ReactElement => (
-    <ShowController {...props}>
-        {(controllerProps : any) : React.ReactElement => (
-            <ShowView
-                title={<Title record={props.record} />}
-                {...controllerProps}
-                {...props}>
-                <SimpleShowLayout>
-                    <RichTextField
-                        label=''
-                        source='description' />
-                    {controllerProps && controllerProps.record && controllerProps.record.images && controllerProps.record.images.length > 0 && (
-                        <ArrayField
-                            label=''
-                            source='images'>
-                            <Gallery
-                                source='imageUrl'
-                                caption='description'>
-                                <ImageField
+interface IProps {
+    record : any;
+}
+
+export class ItemShow extends React.PureComponent<IProps, {}> {
+    public constructor(props : any) {
+        super(props);
+    }
+
+    public componentDidUpdate : () => void = () : void => {
+        window.scrollTo(0, 0);
+    }
+
+    public render() : React.ReactElement {
+        return (
+            <ShowController {...this.props}>
+                {(controllerProps : any) : React.ReactElement => (
+                    <ShowView
+                        title={<Title record={this.props.record} />}
+                        {...controllerProps}
+                        {...this.props}>
+                        <SimpleShowLayout>
+                            <RichTextField
+                                label=''
+                                source='description' />
+                            {controllerProps && controllerProps.record && controllerProps.record.images && controllerProps.record.images.length > 0 && (
+                                <ArrayField
                                     label=''
-                                    source='imageUrl'
-                                    title='description' />
-                            </Gallery>
-                        </ArrayField>
-                    )}
-                    <TextField
-                        label=''
-                        source='source.name' />
-                    <DateField
-                        label=''
-                        source='publishDate'
-                        showTime />
-                </SimpleShowLayout>
-            </ShowView>
-        )}
-    </ShowController>
-);
+                                    source='images'>
+                                    <Gallery
+                                        source='imageUrl'
+                                        caption='description'>
+                                        <ImageField
+                                            label=''
+                                            source='imageUrl'
+                                            title='description' />
+                                    </Gallery>
+                                </ArrayField>
+                            )}
+                            <TextField
+                                label=''
+                                source='source.name' />
+                            <DateField
+                                label=''
+                                source='publishDate'
+                                showTime />
+                        </SimpleShowLayout>
+                    </ShowView>
+                )}
+            </ShowController>
+        );
+    }
+}
