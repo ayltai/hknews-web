2556798d5f1f3d1e855a60fbe441d48ef1f23dcd
Alan Tai <ayltai@gmail.com>
2020-01-22 10:48:45 +0800

    Fix HTML entity decoder error

--- src/util/StringUtils.js
+++ src/util/StringUtils.js
@@ -1,5 +1,7 @@
 export const decode = input => {
-    return input.replace(/&#(\d+);/g, (match, code) => {
-        return String.fromCharCode(code);
-    });
+    return input
+        ? input.replace(/&#(\d+);/g, (match, code) => {
+            return String.fromCharCode(code);
+        })
+        : input;
 };
